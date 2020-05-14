c3de192567ab2585e491789997d2a9269d3b1e47
blackorbit1 <enzo.dutra@gmail.com>
2020-05-14 18:40:17 +0200

    base

---
+++ src/algorithms/DefaultTeam.java
@@ -0,0 +1,72 @@
+package algorithms;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class DefaultTeam {
+	/**
+	 *  permet d'obtenir une matrice donnant pour deux points d'indice i et j l'indice k
+	 *  du prochain sommet dans un plus court chemin de i à j
+	 * 
+	 * @param points
+	 * @param edgeThreshold
+	 * @return une matrice à deux dimensions
+	 */
+	public int[][] calculShortestPaths(ArrayList<Point> points, int edgeThreshold) {
+		int[][] paths=new int[points.size()][points.size()];
+		for (int i=0;i<paths.length;i++) for (int j=0;j<paths.length;j++) paths[i][j]=i;
+
+		double[][] dist=new double[points.size()][points.size()];
+
+		for (int i=0;i<paths.length;i++) {
+			for (int j=0;j<paths.length;j++) {
+				if (i==j) {dist[i][i]=0; continue;}
+				if (points.get(i).distance(points.get(j))<=edgeThreshold) dist[i][j]=points.get(i).distance(points.get(j)); else dist[i][j]=Double.POSITIVE_INFINITY;
+				paths[i][j]=j;
+			}
+		}
+
+		for (int k=0;k<paths.length;k++) {
+			for (int i=0;i<paths.length;i++) {
+				for (int j=0;j<paths.length;j++) {
+					if (dist[i][j]>dist[i][k] + dist[k][j]){
+						dist[i][j]=dist[i][k] + dist[k][j];
+						paths[i][j]=paths[i][k];
+
+					}
+				}
+			}
+		}
+
+		return paths; // test 
+	}
+	public Tree2D calculSteiner(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
+		//REMOVE >>>>>
+		Tree2D leafX = new Tree2D(new Point(700,400),new ArrayList<Tree2D>());
+		Tree2D leafY = new Tree2D(new Point(700,500),new ArrayList<Tree2D>());
+		Tree2D leafZ = new Tree2D(new Point(800,450),new ArrayList<Tree2D>());
+		ArrayList<Tree2D> subTrees = new ArrayList<Tree2D>();
+		subTrees.add(leafX);
+		subTrees.add(leafY);
+		subTrees.add(leafZ);
+		Tree2D steinerTree = new Tree2D(new Point(750,450),subTrees);
+		//<<<<< REMOVE
+
+		return steinerTree;
+	}
+	
+	public Tree2D calculSteinerBudget(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
+		//REMOVE >>>>>
+		Tree2D leafX = new Tree2D(new Point(700,400),new ArrayList<Tree2D>());
+		Tree2D leafY = new Tree2D(new Point(700,500),new ArrayList<Tree2D>());
+		Tree2D leafZ = new Tree2D(new Point(800,450),new ArrayList<Tree2D>());
+		ArrayList<Tree2D> subTrees = new ArrayList<Tree2D>();
+		subTrees.add(leafX);
+		subTrees.add(leafY);
+		subTrees.add(leafZ);
+		Tree2D steinerTree = new Tree2D(new Point(750,450),subTrees);
+		//<<<<< REMOVE
+
+		return steinerTree;
+	}
+}
